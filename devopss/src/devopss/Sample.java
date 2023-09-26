6dfd9b13b589c8e701d1f8c38f79b7ae5c02dfb3
Admin <Admin@DESKTOP-1E4QUA6>
2023-09-26 15:30:38 -0700

    Integration of GitHub

---
+++ devopss/src/devopss/Sample.java
@@ -0,0 +1,11 @@
+package devopss;
+
+public class Sample {
+
+	public static void main (String [] args) {
+			//TODO Auto-generated method stub
+	System.out.println("Hi iam Analysing the code");
+	System.out.println("Hi iam Analysing the code");
+	System.out.println("Hi iam Analysing the code");
+	}
+}
